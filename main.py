0b378e193b120b6469cd1677b49ee369c2d4e760
avishaya <avishaya67@gmail.com>
2020-08-19 18:38:13 +0300

    weighting the vertexes instead of the edges + print graph accordingly

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ def skip_redundant_spaces_in_list(items_list):
 
 
 def get_edges(nodes):
-    print("Edges format is <Vertex>, <Vertex>, <Weight>. once done, please type done.")
+    print("Edges format is <Vertex>, <Vertex>. once done, please type done.")
     edges_input = input("Enter edge : ")
     edges = []
     while edges_input != skip_redundant_spaces_in_str('done'):
         edge_formatted = skip_redundant_spaces_in_list(edges_input.split(','))
-        if len(edge_formatted) != 3:
-            print("Edges format is <Vertex>, <Vertex>, <Weight>. Please try again.")
+        if len(edge_formatted) != 2:
+            print("Edges format is <Vertex>, <Vertex>. Please try again.")
         elif edge_formatted[0] not in nodes or edge_formatted[1] not in nodes:
             print("No such vertexes. Please try again.")
         else:
@@ -30,35 +30,45 @@ def get_edges(nodes):
 
 
 def get_nodes():
-    nodes_input = input("Enter desired nodes divided by comma : ")
-    return skip_redundant_spaces_in_list(nodes_input.split(','))
+    nodes_input = input("Enter desired nodes <node>:<weight>, <node>:<weight>, ... : ")
+    nodes_seperated = skip_redundant_spaces_in_list(nodes_input.split(','))
+    weighted_nodes = {}
+    for node in nodes_seperated:
+        split_node = node.split(':')
+        weighted_nodes[skip_redundant_spaces_in_str(split_node[0])] = int(skip_redundant_spaces_in_str(split_node[1]))
+    return weighted_nodes
 
 
 def print_edges(graph):
-    for u, v, w in graph.edges.data('weight'):
-        print(f'({u},{v}) --> {w}')
+    for u, v in graph.edges:
+        print(f'({u} -- {v})')
 
 
 def print_graph(graph):
     pos = nx.spring_layout(graph)
-    nx.draw(graph, pos, with_labels=True)
-    edge_labels = {(u, v): d for u, v, d in graph.edges}
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
+    nx.draw(graph, pos)
+    #edge_labels = {(u, v): d for u, v, d in graph.edges}
+    vertex_weight = {u: f'{u}:{w["weight"]}'  for u, w in graph.nodes.data()}
+    nx.draw_networkx_labels(graph, pos, labels=vertex_weight)
     plt.show()
 
 
 def main():
-    graph = nx.MultiGraph()
+    graph = nx.Graph()
 
     # nodes = get_nodes()
-    # graph.add_nodes_from(nodes)
+    # for name, weight in nodes.items():
+    #     graph.add_node(name, weight=weight)
     # graph.add_edges_from(get_edges(nodes))
     # mock
-    graph.add_nodes_from(['a','b','c'])
-    graph.add_edges_from([('a','b',2), ('a','c',3)])
+    nodes = {'a':2, 'b':3, 'c':5, 'd':1, 'e':2, 'f':3}
+    for name, weight in nodes.items():
+        graph.add_node(name, weight=weight)
+    graph.add_edges_from([('a','b'), ('a','c'), ('a','d'), ('b','c'), ('b','d'), ('d','e'), ('e','f')])
 
-    print(graph.nodes)
-    print_edges(graph)
+    # debug
+    # print(graph.nodes)
+    # print_edges(graph)
 
     print_graph(graph)
 
