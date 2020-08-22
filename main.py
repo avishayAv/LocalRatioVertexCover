aa9b9b686f67a8384da63c2402ceb80bfbd6910b
avishaya <avishaya67@gmail.com>
2020-08-22 17:12:29 +0300

    Add features : edge coloring + difficulty

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ UNMARKED_VERTEX = '#FAEBD7'
 MARKED_VERTEX = 'red'
 WEIGHT = 'weight'
 VERTEX_COVER = '#00FF00'
+MARKED_EDGE = 'white'
+UNMARKED_EDGE = 'black'
 
 
 class Graph:
-    def __init__(self, graph, color_map):
+    def __init__(self, graph, node_color_map, edge_color_map):
         self.graph = graph
-        self.color_map = color_map
+        self.node_color_map = node_color_map
+        self.edge_color_map = edge_color_map
 
 
 def skip_redundant_spaces_in_str(str):
@@ -51,22 +54,21 @@ def get_nodes():
     return weighted_nodes
 
 
-def print_edges(graph):
-    for u, v in graph.edges:
-        print(f'({u} -- {v})')
-
-
-def print_graph(Graphs):
+def print_graph(Graphs, difficulty):
+    difficulty_to_seconds = {'slow': 3, 'medium': 2, 'fast': 1}
     plt.figure(1)
     for index, graph in enumerate(Graphs):
         pos = nx.circular_layout(graph.graph)
         nx.draw(graph.graph, pos)
         vertex_weight = {u: f'{u}:{w[WEIGHT]}' for u, w in graph.graph.nodes.data()}
-        nx.draw_networkx_nodes(graph.graph, pos, node_color=graph.color_map)
+        nx.draw_networkx_nodes(graph.graph, pos, node_color=graph.node_color_map)
+        nx.draw_networkx_edges(graph.graph, pos, edge_color=graph.edge_color_map)
         nx.draw_networkx_labels(graph.graph, pos, labels=vertex_weight)
-        plt.pause(2)
-        plt.clf()
+        if index != len(Graphs)-1:
+            plt.pause(difficulty_to_seconds[difficulty])
+            plt.clf()
     plt.show()
+
 
 def color_vertex_participate_in_edge(nodes, u, v):
     color_map = []
@@ -88,22 +90,32 @@ def color_vertex_in_cover(nodes):
     return color_map
 
 
+def remove_edges_from_taken_vertex(graph, u, v):
+    color_map = []
+    for a,b in graph.edges:
+        if graph.nodes.data()[a][WEIGHT] == 0 or graph.nodes.data()[b][WEIGHT] == 0:
+            color_map.append(MARKED_EDGE)
+        else:
+            color_map.append(UNMARKED_EDGE)
+    return color_map
+
+
 def local_ratio_vertex_cover(Graphs, graph):
     # before
-    Graphs.append(Graph(graph, [UNMARKED_VERTEX for node in graph.nodes]))
+    Graphs.append(Graph(graph, [UNMARKED_VERTEX for node in graph.nodes], [UNMARKED_EDGE for edge in graph.edges]))
     # iterate
     for u, v in graph.edges:
-        Graphs.append(Graph(graph, color_vertex_participate_in_edge(graph.nodes, u, v)))
+        Graphs.append(Graph(graph, color_vertex_participate_in_edge(graph.nodes, u, v), remove_edges_from_taken_vertex(graph, u, v)))
         curr_graph = graph.copy()
         epsilon = min(curr_graph.nodes[u][WEIGHT], curr_graph.nodes[v][WEIGHT])
         curr_graph.nodes[u][WEIGHT] -= epsilon
         curr_graph.nodes[v][WEIGHT] -= epsilon
-        # print_graph(graph)
-        Graphs.append(Graph(curr_graph, color_vertex_participate_in_edge(graph.nodes, u, v)))
+        Graphs.append(Graph(curr_graph, color_vertex_participate_in_edge(curr_graph.nodes, u, v), remove_edges_from_taken_vertex(graph, u, v)))
+        Graphs.append(Graph(curr_graph, [UNMARKED_VERTEX for node in graph.nodes], remove_edges_from_taken_vertex(graph, u, v)))
         graph = curr_graph
     # result
-    Graphs.append(Graph(graph, [UNMARKED_VERTEX for node in graph.nodes]))
-    Graphs.append(Graph(graph, color_vertex_in_cover(graph.nodes)))
+    Graphs.append(Graph(graph, [UNMARKED_VERTEX for node in graph.nodes], remove_edges_from_taken_vertex(graph, u, v)))
+    Graphs.append(Graph(graph, color_vertex_in_cover(graph.nodes), remove_edges_from_taken_vertex(graph, u, v)))
 
 
 def main():
@@ -114,19 +126,17 @@ def main():
     # for name, weight in nodes.items():
     #     graph.add_node(name, weight=weight)
     # graph.add_edges_from(get_edges(nodes))
+    # difficulty = input("Enter desired difficulty (slow/medium/fast):")
     # mock
     nodes = {'a':2, 'b':3, 'c':5, 'd':1, 'e':2, 'f':3}
     for name, weight in nodes.items():
         graph.add_node(name, weight=weight)
     graph.add_edges_from([('a','b'), ('a','c'), ('a','d'), ('b','d'), ('d','e'), ('e','f')])
-
-    # debug
-    # print(graph.nodes)
-    # print_edges(graph)
+    difficulty = 'fast'
 
     local_ratio_vertex_cover(Graphs, graph)
 
-    print_graph(Graphs)
+    print_graph(Graphs, difficulty)
 
 
 if __name__ == '__main__':
